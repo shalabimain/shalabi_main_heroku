f1eb432b0b966bb5523e310998898e22101913b1
shalabimain <m_shalabi2@fanshaweonline.ca>
2019-03-22 15:37:14 -0700

    last updated files, responsive, etc..

--- data.js
+++ data.js
@@ -1,21 +1,21 @@
 const data = {
-    name: "Connor",
+    name: "Main",
     role: "the role",
 
     user1: {
-        name: "Jordans B-Ball Shoes",
-        role: "Basketball Shoes",
-        bio: "New Jordan 13 shoes in Red",
-        other: "Price: $249.99",
-        bioPic: "images/bball1.png"
+        name: "Red and pink Rose",
+        
+        bio: "Pink –  A pink rose is a classic symbol of grace and elegance and is often given as a token of admiration and appreciation. Available in dark and lighter pinks but regardless of the shade, this vibrant roses create a sumptuous bouquet. Cream – Is there a special friend or loved family member going through a tough time? A cream rose lets them know you are thinking about them. Charm and thoughtfulness are the language of cream roses."
+        ,
+        other: "Price: $40.00",
+        bioPic: "images/rose3.jpg"
     },
     
     user2: {
-        name: "Nike 95' Shoes",
-        role: "Running Shoes",
-        bio: "Nike SB 95's in all black",
-        other: "Price: $149.99",
-        bioPic: "images/running1.png"
+        name: "red roses",
+        bio: "Roses have inspired people over the ages to develop a language of roses by ascribing meanings to color, variety and number of roses being gifted. Traditionally, the rose is considered the flower of love. Nothing beats red roses in saying I Love You on Valentine's Day.",
+        other: "Price: $25.75",
+        bioPic: "images/rose2.jpg"
     }
 };
 
